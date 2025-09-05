52de3a76cc1b6186867a58e06fbe9004c8392739
Vito Andareas Manik <vitoandareas15@gmail.com>
2025-09-05 22:12:48 +0800

    fix: disable CDN caching in Sanity client configuration

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,7 +3,7 @@ import { createClient } from "@sanity/client";
 export const sanityClient = createClient({
 	projectId: process.env.SANITY_PROJECT_ID,
 	dataset: process.env.SANITY_DATASET,
-	useCdn: true,
+	useCdn: false,
 	apiVersion: process.env.SANITY_API_VERSION,
 	token: process.env.SANITY_SECRET_TOKEN, // Needed for certain operations like updating content, accessing drafts or using draft perspectives
 });
